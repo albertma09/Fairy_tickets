ba331d5d001de142b895970e853bd485f7d5ebdf
Aitor Lopera <alopera@alumnat.copernic.cat>
2023-12-19 16:19:02 +0100

    buscador en home.index funcional

--- fairy_tickets/resources/views/components/search-component.blade.php
+++ fairy_tickets/resources/views/components/search-component.blade.php
@@ -1,4 +1,6 @@
-<form action="get">
+<form action="{{ route('home.index') }}" method="post">
+    @csrf
     <label for="search-input" class="icon-button"><i class="fa-solid fa-magnifying-glass"></i></label>
     <input type="text" id="search-input" name="search-input" />
+    <input type="submit" value="Busca">
 </form>
