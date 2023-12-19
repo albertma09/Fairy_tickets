5f4bcc2e3071ee131520405b62b14230a85ed7da
Aitor Lopera <alopera@alumnat.copernic.cat>
2023-12-19 15:52:43 +0100

    Instalación de Sass hecha, inicio estilos navegador

--- fairy_tickets/resources/views/components/navigation-component.blade.php
+++ fairy_tickets/resources/views/components/navigation-component.blade.php
@@ -2,10 +2,11 @@
     <a href="{{ route('home.index') }}">
         <img src="{{ asset('logo/logoFairyTickets_fondoOscuro.png') }}" alt="Logo del sitio web" />
     </a>
+    <button class="icon-button"></button>
     <x-search-component />
     <ul>
         <li><a href="{{ route('home.index') }}">Home</a></li>
     </ul>
-    <button><i class="fa-solid fa-user"></i></button>
-    <button><i class="fa-solid fa-bars"></i></button>
+    <button class="icon-button"><i class="fa-solid fa-user"></i></button>
+    <button class="icon-button"><i class="fa-solid fa-bars"></i></button>
 </nav>
