57ec910afef9bc49de7f55020aa62761ae34ab1a
alopera <alopera@alumnat.copernic.cat>
2024-01-15 19:38:44 +0100

    Terminado refactor sass, unificación botones, ahora todo funciona con clases .button y button-brand o .button-dark

--- fairy_tickets/resources/views/auth/login.blade.php
+++ fairy_tickets/resources/views/auth/login.blade.php
@@ -5,10 +5,7 @@
 @section('content')
     <form method="POST" action="{{ route('login') }}">
         @csrf
-        <input type="email" name="email" placeholder="Correo" required>
-        <br>
-        <input type="password" name="password" placeholder="Contraseña" required>
-        <br>
+        <input type="email" name="email" placeholder="Correo" required />
         <button class="button button-brand" type="submit">Iniciar Sesión</button>
         @if (session('error'))
             <p class="msg-error">{{ session('error') }}</p>
