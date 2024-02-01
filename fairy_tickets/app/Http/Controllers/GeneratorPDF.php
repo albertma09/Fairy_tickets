001f9754fdfa7a80c81c0d3c47aa6275eaa233c7
amateos <amateos@alumnat.copernic.cat>
2024-02-01 20:11:35 +0100

    crear generador de pdf para la compra de tickets(solo esta la plantilla, no es funcional)

--- fairy_tickets/app/Http/Controllers/GeneratorPDF.php
+++ fairy_tickets/app/Http/Controllers/GeneratorPDF.php
@@ -3,14 +3,32 @@
 namespace App\Http\Controllers;
 
 use SimpleSoftwareIO\QrCode\Facades\QrCode;
+use Barryvdh\DomPDF\Facade\Pdf;
+use Dompdf\Dompdf;
 class GeneratorPDF extends Controller
 {
     
 
 public function generatePDF()
 {
-    $codigoQR = QrCode::generate('prueba');
-    return view('components.ticket-pdf',['codigoQR' => $codigoQR]);
+
+
+    $codigoQR = base64_encode(QrCode::format('svg')->size(200)->errorCorrection('H')->generate('string'));
+
+
+    $numeroPaginas = 5;
+
+    $html = '';
+
+    for ($i = 0; $i < $numeroPaginas; $i++) {
+        $html .= view('components.ticket-pdf', ['codigoQR' => $codigoQR])->render();
+    }
+
+
+    $pdf = Pdf::loadHtml($html);
+
+
+     return $pdf->stream('ticket.pdf');
 }
 
 }
