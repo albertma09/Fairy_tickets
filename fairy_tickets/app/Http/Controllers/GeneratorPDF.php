75d3988257ebb7442a675d22cf2232e6bf871e9b
amateos <amateos@alumnat.copernic.cat>
2024-02-02 17:43:24 +0100

    generador de pdf finalizado, realizar consulta con id session y correo del comprador y devuelve los tickets comprados para esa sesion

--- fairy_tickets/app/Http/Controllers/GeneratorPDF.php
+++ fairy_tickets/app/Http/Controllers/GeneratorPDF.php
@@ -2,33 +2,48 @@
 
 namespace App\Http\Controllers;
 
-use SimpleSoftwareIO\QrCode\Facades\QrCode;
-use Barryvdh\DomPDF\Facade\Pdf;
+use DateTime;
 use Dompdf\Dompdf;
+use App\Models\Ticket;
+use Barryvdh\DomPDF\Facade\Pdf;
+use SimpleSoftwareIO\QrCode\Facades\QrCode;
+
 class GeneratorPDF extends Controller
 {
-    
-
-public function generatePDF()
-{
 
 
-    $codigoQR = base64_encode(QrCode::format('svg')->size(200)->errorCorrection('H')->generate('string'));
+    public function generatePDF()
+    {
+        $tickets = Ticket::getTicketsInformation();
 
 
-    $numeroPaginas = 5;
 
-    $html = '';
 
-    for ($i = 0; $i < $numeroPaginas; $i++) {
-        $html .= view('components.ticket-pdf', ['codigoQR' => $codigoQR])->render();
+
+        $html = '';
+
+        foreach ($tickets as $ticket) {
+            $codigoQR = base64_encode(QrCode::format('svg')->size(200)->errorCorrection('H')->generate($ticket->id));
+            $date = $ticket->date; // Suponiendo que 'date' es el nombre de la columna que contiene la fecha
+
+            // Convertir la fecha a un objeto DateTime para poder manipularla
+            $dateObj = new DateTime($date);
+
+            // Obtener el día, el mes y el año por separado
+            $day = $dateObj->format('d'); // 'd' devuelve el día del mes con ceros iniciales (01 a 31)
+            $month = $dateObj->format('m'); // 'm' devuelve el número del mes (01 para enero, 02 para febrero, etc.)
+            $year = $dateObj->format('Y'); // 'Y' devuelve el año en formato de 4 dígitos (ej. 2024)
+            $html .= view('components.ticket-pdf', ['codigoQR' => $codigoQR, 'ticket' => $ticket, 'day'=>$day, 'month'=>$month, 'year'=>$year])->render();
+        }
+
+
+
+
+
+
+        $pdf = Pdf::loadHtml($html);
+
+
+        return $pdf->stream('ticket.pdf');
     }
-
-
-    $pdf = Pdf::loadHtml($html);
-
-
-     return $pdf->stream('ticket.pdf');
-}
-
 }
--- fairy_tickets/app/Models/Ticket.php
+++ fairy_tickets/app/Models/Ticket.php
@@ -2,9 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Exception;
+use Illuminate\Support\Facades\Log;
 
 class Ticket extends Model
 {
@@ -18,5 +21,42 @@ class Ticket extends Model
     public function ticketType(): BelongsTo
     {
         return $this->belongsTo(TicketType::class);
+    }
+
+    public static function getTicketsInformation()
+    {
+
+        try{
+            $tickets = DB::table('tickets')
+            ->select(
+                'tickets.id',
+                'tickets.name as client_name',
+                'tickets.dni',
+                'ticket_types.description as ticket_type_name',
+                'ticket_types.price',
+                'events.name as event_name',
+                'events.description as event_description',
+                'sessions.date',
+                'sessions.hour',
+                'locations.name as location_name',
+                'locations.city',
+                'locations.province'
+            )
+            ->join('purchases', 'purchases.id', '=', 'tickets.purchase_id')
+            ->join('ticket_types', 'ticket_types.id', '=', 'tickets.ticket_type_id')
+            ->join('sessions', 'ticket_types.session_id', '=', 'sessions.id')
+            ->join('events', 'events.id', '=', 'sessions.event_id')
+            ->join('locations', 'locations.id', '=', 'events.location_id')
+            ->where('purchases.email', '=', 'avis.beahan@example.org')
+            ->where('ticket_types.session_id', '=', 234)
+            ->get();
+
+            return $tickets;
+        }catch (Exception $e) {
+            Log::debug($e->getMessage());
+        }
+        
+
+
     }
 }
