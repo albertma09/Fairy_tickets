8719340c870d3c375727437d4def35ceaa7d7818
Aitor Lopera <alopera@alumnat.copernic.cat>
2024-02-05 20:14:14 +0100

    Cambio en la barra de categorías, corrección del parámetro de búsqueda

--- fairy_tickets/resources/views/components/category-filter-component.blade.php
+++ fairy_tickets/resources/views/components/category-filter-component.blade.php
@@ -1,22 +1,22 @@
 <div class="filter-categories">
-    <div class="category-card-more"><a href="{{ route('searchByCategory.index', ['name' => 'cine']) }}"
+    <div class="category-card-more"><a href="{{ route('searchByCategory.index', ['name' => 'Cine']) }}"
             class="button-category">Cine</a></div>
-    <div class="category-card-more"><a href="{{ route('searchByCategory.index', ['name' => 'comedia']) }}"
+    <div class="category-card-more"><a href="{{ route('searchByCategory.index', ['name' => 'Comedia']) }}"
             class="button-category">Comedia</a></div>
-    <div class="category-card-more"><a href="{{ route('searchByCategory.index', ['name' => 'conferencia']) }}"
+    <div class="category-card-more"><a href="{{ route('searchByCategory.index', ['name' => 'Conferencia']) }}"
             class="button-category">Conferencia</a></div>
-    <div class="category-card-more"><a href="{{ route('searchByCategory.index', ['name' => 'danza']) }}"
+    <div class="category-card-more"><a href="{{ route('searchByCategory.index', ['name' => 'Danza']) }}"
             class="button-category">Danza</a></div>
-    <div class="category-card-more"><a href="{{ route('searchByCategory.index', ['name' => 'hogar']) }}"
+    <div class="category-card-more"><a href="{{ route('searchByCategory.index', ['name' => 'Hogar']) }}"
             class="button-category">Hogar</a></div>
-    <div class="category-card-more"><a href="{{ route('searchByCategory.index', ['name' => 'moda']) }}"
+    <div class="category-card-more"><a href="{{ route('searchByCategory.index', ['name' => 'Moda']) }}"
             class="button-category">Moda</a></div>
-    <div class="category-card-more"><a href="{{ route('searchByCategory.index', ['name' => 'musica']) }}"
+    <div class="category-card-more"><a href="{{ route('searchByCategory.index', ['name' => 'Musica']) }}"
             class="button-category">Musica</a></div>
-    <div class="category-card-more"><a href="{{ route('searchByCategory.index', ['name' => 'opera']) }}"
+    <div class="category-card-more"><a href="{{ route('searchByCategory.index', ['name' => 'Opera']) }}"
             class="button-category">Opera</a></div>
-    <div class="category-card-more"><a href="{{ route('searchByCategory.index', ['name' => 'salud']) }}"
+    <div class="category-card-more"><a href="{{ route('searchByCategory.index', ['name' => 'Salud']) }}"
             class="button-category">Salud</a></div>
-    <div class="category-card-more"><a href="{{ route('searchByCategory.index', ['name' => 'teatro']) }}"
+    <div class="category-card-more"><a href="{{ route('searchByCategory.index', ['name' => 'Teatro']) }}"
             class="button-category">Teatro</a></div>
 </div>
