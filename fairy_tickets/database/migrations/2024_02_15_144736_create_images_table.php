651ad34220672f7816cc11ef56fafd7c219a0523
Aitor Lopera <alopera@alumnat.copernic.cat>
2024-02-19 20:03:10 +0100

    BD: añadida tabla images

---
+++ fairy_tickets/database/migrations/2024_02_15_144736_create_images_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('event_id');
+            
+            // atributos iniciales BD
+            $table->string('small');
+            $table->string('medium');
+            $table->string('big');
+            $table->boolean('main');
+
+            // timestamps
+            $table->timestamps();
+
+            // definicion constraint FK
+            $table->foreign('event_id')->references('id')->on('events')->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('images');
+    }
+};
--- fairy_tickets/database/seeders/DatabaseSeeder.php
+++ fairy_tickets/database/seeders/DatabaseSeeder.php
@@ -18,12 +18,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
 
         if ($this->command->confirm('Desea refrescar la base de datos?', true)) {
             $this->command->call('migrate:fresh');
