4ed7ffd9433e3245ef81a870938b000024edcbc5
alopera <alopera@alumnat.copernic.cat>
2024-03-03 15:56:37 +0100

    API funcionando con seguridad

--- api/ImgAda/app/Services/Validator.php
+++ api/ImgAda/app/Services/Validator.php
@@ -4,43 +4,62 @@ namespace App\Services;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Crypt;
+use Exception;
 
 class Validator
 {
-   public static function validateStoreRequest(Request $request)
-   {
-      Log::info("Llamada al método Validator.validateStoreRequest con: $request");
-      $errors = [];
+    public static function validateStoreRequest(Request $request)
+    {
+        Log::info("Llamada al método Validator.validateStoreRequest con: $request");
+        $errors = [];
 
-      // Comprobamos si el parámetro 'image' contiene algun archivo
-      if (!$request->hasFile('image')) {
-         $errors[] = 'No se ha recibido ninguna imagen';
-      }
+        // Comprobamos si la request lleva el parámetro 'sizes' con algun valor
+        if (!$request->filled('pwd')) {
+            $errors[] = 'Falta el parámetro "pwd" con las credenciales de la API';
+        }
 
-      // Comprobamos si la request lleva el parámetro 'sizes' con algun valor
-      if (!$request->filled('sizes')) {
-         $errors[] = 'Falta el parámetro "sizes" con los tamaños de reescalado deseados';
-      }
+        // Comprobamos si el parámetro 'image' contiene algun archivo
+        if (!$request->hasFile('image')) {
+            $errors[] = 'No se ha recibido ninguna imagen';
+        }
 
-      // Validamos que el parámetro 'sizes' sea un array y contenga valores numéricos
-      $sizes = $request->input('sizes');
+        // Comprobamos si la request lleva el parámetro 'sizes' con algun valor
+        if (!$request->filled('sizes')) {
+            $errors[] = 'Falta el parámetro "sizes" con los tamaños de reescalado deseados';
+        }
+        return $errors;
+    }
 
-      // Miramos si 'sizes' es un string, en vez de array, si es así lo convertimos a array
-      if (is_string($sizes)) {
-         $sizesArray = json_decode($sizes, true);
-
-         if (!is_array($sizesArray)) {
-            $errors[] = "'Sizes' ha de ser enviado como un array numérico.";
-            
-         } else {
-            foreach ($sizesArray as $size) {
-               if (!is_numeric($size)) {
-                  $errors[] = "El tamaño '$size' en 'sizes' no es un número válido.";
-               }
+    // Función que comprueba el formato del parámetro 'sizes' se la request
+    public static function validateSizesInput($sizes): array
+    {
+        try {
+            $errors = [];
+            if (!is_array($sizes)) {
+                $errors[] = "'Sizes' ha de ser enviado como un array numérico.";
+            } else {
+                foreach ($sizes as $size) {
+                    if (!is_numeric($size)) {
+                        $errors[] = "El tamaño '$size' en 'sizes' no es un número válido.";
+                    }
+                }
             }
-         }
-      }
-      return $errors;
-   }
+            return $errors;
+        } catch (\Exception $ex) {
+            Log::error($ex->getMessage());
+            throw new Exception($ex->getMessage(), 500);
+        }
+    }
+
+    // Función que compara unas credenciales con las guardads en las variables de entorno
+    public static function verifyCredentials(string $credentials): bool
+    {
+        try {
+            Log::info("verifyCredentials: $credentials");
+            return ($credentials == env('API_REQUESTS_KEY'));
+        } catch (\Exception $ex) {
+            Log::error($ex->getMessage());
+            throw new Exception($ex->getMessage(), 500);
+        }
+    }
 }
