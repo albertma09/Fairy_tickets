70c090366ae70829d3d8bed782a28ebb705b6c2b
Diego <dyamador@alumnat.copernic.cat>
2024-03-03 16:23:55 +0100

    metadatos para los dos layouts implementados descripción para navegador y redes sociales asociadas a 'facebook'

--- fairy_tickets/resources/views/layouts/master.blade.php
+++ fairy_tickets/resources/views/layouts/master.blade.php
@@ -5,6 +5,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description"
+        content="Fairy Tickets, vive tus eventos favoritos como si estuvieras en un cuento de hadas. 
+        Encuentra los mejores eventos a los mejores precios que se realizarán en tu ciudad, no te quedes
+        sin tu entrada." />
+    <meta property="og:description"
+        content="Fairy Tickets, vive tus eventos favoritos como si estuvieras en un cuento de hadas. 
+        Encuentra los mejores eventos a los mejores precios que se realizarán en tu ciudad, no te quedes
+        sin tu entrada." />
     <title>@yield('title')</title>
     <script src="https://kit.fontawesome.com/918614923c.js" crossorigin="anonymous"></script>
     <link rel="shortcut icon" type="image/png" href="{{ asset('/logo/favicon.png') }}">
@@ -26,9 +34,9 @@
                 <x-footer-component />
             </footer>
         </div>
-        
+
     </div>
-    
+
     <script type="module" src="{{ asset('js/main.js') }}"></script>
     @yield('scripts')
 </body>
